741e97cd05b5a089b8b3e6ddbd190f26cca973dc
haizhao <1013264065@qq.com>
2019-12-05 18:19:38 +0800

    init

---
+++ packages/config/dynamic.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <el-form-item label="对齐方式">
+      <el-select v-model="data.children.align"
+                 placeholder="对齐方式">
+        <el-option label="居左"
+                   value="left"></el-option>
+        <el-option label="居中"
+                   value="center"></el-option>
+        <el-option label="居右"
+                   value="right"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="头部对齐方式">
+      <el-select v-model="data.children.headerAlign"
+                 placeholder="对齐方式">
+        <el-option label="居左"
+                   value="left"></el-option>
+        <el-option label="居中"
+                   value="center"></el-option>
+        <el-option label="居右"
+                   value="right"></el-option>
+      </el-select>
+    </el-form-item>
+    <el-form-item label="是否禁用">
+      <el-switch v-model="data.disabled"></el-switch>
+    </el-form-item>
+    <el-form-item label="是否可见">
+      <el-switch v-model="data.display"></el-switch>
+    </el-form-item>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "config-dynamic",
+  props: ['data']
+}
+</script>
